c8706f1a33662eb82ed1f0d1e5c757ad5353f41e
humayra akter <humayraakter25@gmail.com>
2023-07-21 10:17:04 +0600

    first commit

---
+++ src/components/UI/AllNews.js
@@ -0,0 +1,111 @@
+import { Card, Col, Row } from "antd";
+import Image from "next/image";
+import {
+  ArrowRightOutlined,
+  CalendarOutlined,
+  CommentOutlined,
+  ProfileOutlined,
+} from "@ant-design/icons";
+import Link from "next/link";
+
+const AllNews = ({ allNews }) => {
+  const { Meta } = Card;
+  return (
+    <>
+      <h1
+        style={{
+          textAlign: "center",
+          fontSize: "50px",
+          margin: " 30px 0px",
+        }}
+      >
+        #TODAY HIGHLIGHT
+      </h1>
+      <Row
+        gutter={{
+          xs: 8,
+          sm: 16,
+          md: 24,
+          lg: 32,
+        }}
+      >
+        {allNews.map((news) => (
+          <Col key={news.id} className="gutter-row" span={6}>
+            <Card
+              hoverable
+              cover={
+                <Image
+                  src={news?.image_url}
+                  width={500}
+                  height={200}
+                  responsive
+                  alt="news image"
+                />
+              }
+            >
+              <Meta title={news?.title} />
+              <div
+                className="line"
+                style={{
+                  height: "5px",
+                  margin: "20px 0",
+                  width: "100%",
+                  background: "#000",
+                }}
+              ></div>
+              <p
+                style={{
+                  display: "flex",
+                  margin: "10px 0",
+                  width: "100%",
+                  color: "gray",
+                  justifyContent: "space-between",
+                }}
+              >
+                <span>
+                  <CalendarOutlined /> {news?.release_date}
+                </span>
+                <span>
+                  <CommentOutlined /> {news?.comment_count} COMMENTS
+                </span>
+                <span>
+                  <ProfileOutlined /> {news?.category}
+                </span>
+              </p>
+
+              <p
+                style={{
+                  fontSize: "15px",
+                }}
+              >
+                {news?.description.length > 100
+                  ? news?.description.slice(0, 70) + "..."
+                  : news?.description}
+              </p>
+
+              <Link href={`/news/${news?.id}`}>
+                <p
+                  style={{
+                    fontSize: "15px",
+                    marginTop: "20px",
+                    width: "100%",
+                    backgroundColor: "black",
+                    color: "white",
+                    padding: "2px 5px",
+                    letterSpacing: "3px",
+                    textAlign: "center",
+                    fontWeight: "300",
+                  }}
+                >
+                  Keep reading <ArrowRightOutlined />
+                </p>
+              </Link>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+};
+
+export default AllNews;
